93ed399e81be8274b52569980eaaa5e8f5c07104
IOUIOU50 <57579709+IOUIOU50@users.noreply.github.com>
2021-07-09 14:40:08 +0900

    RFID인증 및 비밀번호 인증과 통합 완료

--- locker/locker.ino
+++ locker/locker.ino
@@ -18,7 +18,17 @@
 #define PIN_LED_BLUE 6  // sig = dig
 #define RFID_RST 9      // sig = dig, RST  RESET
 #define RFID_SS 10      // sig = dig, SS : SDA
-#define BUTTON_1 7
+/**
+    키패드	핀
+    2	    첫번째 핀
+    1	    두번째 핀
+    3	    네번째 핀
+    4	    세번째 핀
+ */
+#define BUTTON_1 4  // 확인 버튼
+#define BUTTON_2 5  // 취소버튼
+#define BUTTON_3 6  // ??
+#define BUTTON_4 7  // 문 닫힘 버튼
 
 /*********************************
  * 코드에 사용될 변수를 선언합니다.
@@ -29,7 +39,7 @@ int countSec;
 
 //이전 실습에서 알아온 rfid 카드의 일련번호를 아래에 적도록 합니다.
 byte registeredUid[4] = {0x83, 0xD2, 0xE9, 0x18};
-String registeredPw = "1234";
+String registeredPw = "1111";
 
 /*
   센서/모듈 사용을 위한 클래스 변수를 선언합니다.
@@ -52,7 +62,11 @@ void setup() {  // put your setup code here, to run once:
      */
 
     Serial.begin(9600);
-    pinMode(BUTTON_1, INPUT);
+    pinMode(BUTTON_1, INPUT_PULLUP);
+    pinMode(BUTTON_2, INPUT_PULLUP);
+    pinMode(BUTTON_3, INPUT_PULLUP);
+    pinMode(BUTTON_4, INPUT_PULLUP);
+
     servo.attach(PIN_SERVO);
     SPI.begin();      // Init SPI bus
     mfrc.PCD_Init();  // Init MFRC522
@@ -63,8 +77,6 @@ void setup() {  // put your setup code here, to run once:
      *  init variables
      */
     isLocked = true;
-    toggleLockState(isLocked);
-
     /**
      * logs
      */
@@ -93,9 +105,17 @@ void setup() {  // put your setup code here, to run once:
  * ******************************/
 void loop()  // put your main code here, to run repeatedly:
 {
-    /**
-     * 잠겨있을 때
-     */
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    setState(isLocked);
+}
+
+void setState(bool flag) {
+    toggleLockState(flag);
+    if (!flag) {
+        setStateOpened();
+        return;
+    };
     setStateLocked();
 }
 
@@ -105,32 +125,70 @@ void setStateLocked() {
     int pwCursor = 0;
 
     while (isLocked) {
+        /**
+         * RFID로 잠금 해제
+         */
+        if (mfrc.PICC_IsNewCardPresent() && mfrc.PICC_ReadCardSerial()) {
+            bool flag = isVerified(mfrc.uid.uidByte);
+            if (flag) {
+                isLocked = false;
+                return;
+            }
+        }
+
+        /**
+         * 비밀번호로 잠금해제
+         */
         poten = (int)map(analogRead(PIN_POT), 0, 1023, 0, 10);
         lcd.setCursor(0, 0);
         lcd.print("input : ");
+        lcd.setCursor(10, 0);
+        lcd.print("or tag");
+        lcd.setCursor(8, 0);
         lcd.print(poten);
         lcd.setCursor(0, 1);  // 1번 칸, 2번 줄
         lcd.print("password : ");
         for (int i = 0; i < input.length(); i++) {
             lcd.print("*");
         }
-
-        // for (int i = 0; i < input.length(); i++) {
-        //     lcd.print("*");
-        //     lcd.setCursor(i, 1);  // 1번 칸, 2번 줄
-        // }
-
-        if (digitalRead(BUTTON_1)) {
-            delay(300);
+        // true = 1, false = 0. 스위치는 누르면 low(= 0)를 전송
+        if (!digitalRead(BUTTON_2)) {
+            delay(150);
             if (input.length() < 4) {
                 input.concat(String(poten));
                 Serial.println(input);
             } else {
+                if (isPwCorrect(input)) {
+                    isLocked = false;
+                    return;
+                }
+                printWrong();
                 input = "";
                 clearLine(1);
-                input.concat(String(poten));
-                Serial.println(input);
             }
+        }
+        if (!digitalRead(BUTTON_1) && input.length() != 0) {
+            delay(300);
+            input.remove(input.length() - 1);
+            clearLine(1);
+            lcd.print("password : ");
+            
+            for (int i = 0; i < input.length(); i++) {
+                lcd.print("*");
+            }
+            Serial.println(input);
+        }
+    }
+}
+
+bool isPwCorrect(String input) { return registeredPw == input; }
+
+void setStateOpened() {
+    lcd.print("opened!");
+    while (!isLocked) {
+        if (!digitalRead(BUTTON_3)) {
+            isLocked = true;
+            return;
         }
     }
 }
@@ -148,13 +206,14 @@ void toggleLockState(bool state) {
         Serial.print("isLocked : ");
         Serial.println(isLocked);
         servo.write(180);
-        delay(1000);
+        // delay(1000);
         return;
     }
+
     Serial.print("isLocked : ");
     Serial.println(isLocked);
     servo.write(0);
-    delay(1000);
+    // delay(1000);
 }
 
 // 리팩토링 필요
@@ -172,6 +231,9 @@ bool isVerified(byte uid[]) {
         if (registeredUid[i] != uid[i]) flag = false;
         // return false;
     }
+    if(!flag){
+        printWrong();
+    }
     Serial.println(flag);
     return flag;
     // return true;
@@ -183,6 +245,13 @@ void clearLine(int line) {
     lcd.setCursor(0, line);
     lcd.print("                ");
     lcd.setCursor(0, line);
+}
+
+void printWrong(){
+    lcd.setCursor(10, 1);
+    lcd.print("wrong");
+    delay(1500);
+    clearLine(1);
 }
 
 /***********************************************
