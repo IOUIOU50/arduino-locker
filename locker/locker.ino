6ab53dcb5479adde4e36973e5e65e53d0fda393c
IOUIOU50 <57579709+IOUIOU50@users.noreply.github.com>
2021-07-09 11:03:17 +0900

    수정완료 및 LCD개발비밀번호 입력

--- locker/locker.ino
+++ locker/locker.ino
@@ -1,22 +1,24 @@
 /*********************************
  * 라이브러리를 불러옵니다.
  * ******************************/
+#include <LiquidCrystal_I2C.h>
 #include <MFRC522.h>
 #include <SPI.h>
 #include <Servo.h>
-#include <string.h>
+#include <String.h>
+#include <Wire.h>
 
 /*********************************
  * 센서 - 아두이노 연결될 핀번호를 정의합니다.
  * ******************************/
 #define PIN_SERVO 2     // sig = dig
 #define PIN_LED_RED 3   // sig = dig
-#define PIN_LED_BLUE 4  // sig = digj
-#define PIN_POT 5       // sig = dig, POT : potentiometer
+#define PIN_LED_BLUE 4  // sig = dig
+#define PIN_POT 1       // sig = dig, POT : potentiometer
 #define PIN_LED_BLUE 6  // sig = dig
-#define PIN_CDS A1      // CDS = analog
 #define RFID_RST 9      // sig = dig, RST  RESET
 #define RFID_SS 10      // sig = dig, SS : SDA
+#define BUTTON_1 7
 
 /*********************************
  * 코드에 사용될 변수를 선언합니다.
@@ -27,12 +29,14 @@ int countSec;
 
 //이전 실습에서 알아온 rfid 카드의 일련번호를 아래에 적도록 합니다.
 byte registeredUid[4] = {0x83, 0xD2, 0xE9, 0x18};
+String registeredPw = "1234";
 
 /*
-  센서 사용을 위한 클래스 변수를 선언합니다.
+  센서/모듈 사용을 위한 클래스 변수를 선언합니다.
 */
 Servo servo;
 MFRC522 mfrc(RFID_SS, RFID_RST);  // Instance of the class
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 /*********************************
  * dev variables
@@ -43,41 +47,45 @@ String readString;
  * setup함수. 1회 실행.
  * ******************************/
 void setup() {  // put your setup code here, to run once:
-  /**
-   * serial transportation settings
-   */
-  Serial.begin(9600);
-  servo.attach(PIN_SERVO);
-  pinMode(PIN_CDS, INPUT);
-  SPI.begin();      // Init SPI bus
-  mfrc.PCD_Init();  // Init MFRC522
+    /**
+     * serial transportation settings
+     */
 
-  /**
-   *  init variables
-   */
-  if (analogRead(PIN_CDS) > 700) {  // 잠김
+    Serial.begin(9600);
+    pinMode(BUTTON_1, INPUT);
+    servo.attach(PIN_SERVO);
+    SPI.begin();      // Init SPI bus
+    mfrc.PCD_Init();  // Init MFRC522
+    lcd.init();       // LCD 초기화
+    lcd.backlight();
+
+    /**
+     *  init variables
+     */
     isLocked = true;
-  } else {
-    isLocked = false;
-  }
-  countSec = 0;
-  toggleLockState(isLocked);
+    toggleLockState(isLocked);
 
-  /**
-   * logs
-   */
-  Serial.println("arduino starts");
-  Serial.print("registered card uid : ");
-  for (byte i = 0; i < 4; i++) {  // 태그의 ID출력을 ID사이즈 4 까지 반복
-    Serial.print(registeredUid[i],
-                 HEX);  // mfrc.uid.uidByte[0] ~ mfrc.uid.uidByte[3]까지 출력
-    Serial.print(" ");  // id 사이의 간격 출력
-  }
-  Serial.println();
+    /**
+     * logs
+     */
+    Serial.println("arduino starts");
+    Serial.print("registered card uid : ");
+    for (byte i = 0; i < 4; i++) {  // 태그의 ID출력을 ID사이즈 4 까지 반복
+        Serial.print(
+            registeredUid[i],
+            HEX);  // mfrc.uid.uidByte[0] ~ mfrc.uid.uidByte[3]까지 출력
+        Serial.print(" ");  // id 사이의 간격 출력
+    }
 
-  /**
-   * dev settings
-   */
+    Serial.print("registered password : ");
+    for (int i = 0; i < registeredPw.length(); i++) {
+        Serial.print(registeredPw.charAt(i));
+    }
+    Serial.println();
+
+    /**
+     * dev settings
+     */
 }
 
 /*********************************
@@ -85,99 +93,114 @@ void setup() {  // put your setup code here, to run once:
  * ******************************/
 void loop()  // put your main code here, to run repeatedly:
 {
-  /**
-   * 열려있을 때
-   */
-  while (!isLocked)  //! isLocked : case is open
-  {
-    int valCds = analogRead(PIN_CDS);
-    delay(1000);
-    if (valCds > 700) {
-      countSec++;
-      if (countSec > 3) {
-        toggleLockState(true);
-        return;
-      }
-      continue;
-    }
-    countSec = 0;
-
-    return;
-  }
-
-  /**
-   * 잠겨있을 때
-   */
-  if (!mfrc.PICC_IsNewCardPresent() || !mfrc.PICC_ReadCardSerial()) {
-    delay(500);  // 0.5초의 딜레이
-    return;
-  }
-
-  // card 유효성 검증
-  if (isVerified(mfrc.uid.uidByte)) {
-    Serial.println("true");
-    Serial.println("servo toggle");
-    toggleLockState(false);  // false : open / true : closed
-  }
+    /**
+     * 잠겨있을 때
+     */
+    setStateLocked();
 }
 
+void setStateLocked() {
+    String input = "";
+    int poten;
+    int pwCursor = 0;
+
+    while (isLocked) {
+        poten = (int)map(analogRead(PIN_POT), 0, 1023, 0, 10);
+        lcd.setCursor(0, 0);
+        lcd.print("input : ");
+        lcd.print(poten);
+        lcd.setCursor(0, 1);  // 1번 칸, 2번 줄
+        lcd.print("password : ");
+        for (int i = 0; i < input.length(); i++) {
+            lcd.print("*");
+        }
+
+        // for (int i = 0; i < input.length(); i++) {
+        //     lcd.print("*");
+        //     lcd.setCursor(i, 1);  // 1번 칸, 2번 줄
+        // }
+
+        if (digitalRead(BUTTON_1)) {
+            delay(300);
+            if (input.length() < 4) {
+                input.concat(String(poten));
+                Serial.println(input);
+            } else {
+                input = "";
+                clearLine(1);
+                input.concat(String(poten));
+                Serial.println(input);
+            }
+        }
+    }
+}
+bool checkPassword(String pw) {}
+
+bool checkCard() {}
 /***********************************************
  * functions
  ***********************************************/
 
 // toggle degree of servo motor
 void toggleLockState(bool state) {
-  isLocked = state;
-  if (state) {
+    isLocked = state;
+    if (state) {
+        Serial.print("isLocked : ");
+        Serial.println(isLocked);
+        servo.write(180);
+        delay(1000);
+        return;
+    }
     Serial.print("isLocked : ");
     Serial.println(isLocked);
-    servo.write(180);
+    servo.write(0);
     delay(1000);
-    return;
-  }
-  Serial.print("isLocked : ");
-  Serial.println(isLocked);
-  servo.write(0);
-  delay(1000);
 }
 
 // 리팩토링 필요
 bool isVerified(byte uid[]) {
-  // if (sizeof(uid) != sizeof(registeredUid))
-  //   return false;
+    // if (sizeof(uid) != sizeof(registeredUid))
+    //   return false;
 
-  bool flag = true;
-  for (byte i = 0; i < 4; i++) {
-    Serial.print(registeredUid[i]);
-    Serial.print("\t");
-    Serial.print(uid[i]);
-    Serial.println("");
+    bool flag = true;
+    for (byte i = 0; i < 4; i++) {
+        Serial.print(registeredUid[i]);
+        Serial.print("\t");
+        Serial.print(uid[i]);
+        Serial.println("");
 
-    if (registeredUid[i] != uid[i]) flag = false;
-    // return false;
-  }
-  Serial.println(flag);
-  return flag;
-  // return true;
+        if (registeredUid[i] != uid[i]) flag = false;
+        // return false;
+    }
+    Serial.println(flag);
+    return flag;
+    // return true;
+}
+
+void clearLine(int line) {
+    lcd.print("                ");
+    if (line > 1) return;
+    lcd.setCursor(0, line);
+    lcd.print("                ");
+    lcd.setCursor(0, line);
 }
 
 /***********************************************
  * only dev functions
  ***********************************************/
 String readText() {
-  noInterrupts();  // critical section
-  while (Serial.available()) {
-    delay(2);  // delay to allow byte to arrive in input buffer
-    char c = Serial.read();
-    readString += c;
-  }
-  String returnStr = readString;
-  while (Serial.available()) {
-    readString = Serial.readString();
-  }
+    noInterrupts();  // critical section
+    while (Serial.available()) {
+        delay(2);  // delay to allow byte to arrive in input buffer
+        char c = Serial.read();
+        readString += c;
+    }
+    String returnStr = readString;
+    while (Serial.available()) {
+        readString = Serial.readString();
+    }
 
-  readString = "";
-  interrupts();  // release
-  return returnStr;
+    readString = "";
+    interrupts();  // release
+    return returnStr;
 }
-
